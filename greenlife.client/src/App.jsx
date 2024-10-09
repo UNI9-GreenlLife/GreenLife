e839425c6997021764d956b6183d67beb6997eca
Jpsouza3 <jpsouza4032@gmail.com>
2024-10-09 11:36:10 -0300

    Adicionar arquivos de projeto.

---
+++ greenlife.client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Green Life project</h1>
+            <p>Lorem Ipsum</p>
+        </>
+    );
+}
+
+export default App;
