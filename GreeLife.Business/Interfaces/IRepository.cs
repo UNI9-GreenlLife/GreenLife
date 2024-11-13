734ab2f7339228748281708f81a59bffc06f6146
Jpsouza3 <jpsouza4032@gmail.com>
2024-11-12 22:41:11 -0300

    animação de modal e alteração do layout do modal

---
+++ GreeLife.Business/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using GreeLife.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenLife.Business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Create(TEntity entity);
+        Task<TEntity> GetById(int Id);
+        Task<List<TEntity>> GetAll();
+
+        Task Update(TEntity entity);
+        Task Remove(int Id);
+        Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
--- greenlife.client/src/DahsboardPages/EmployeesPage.jsx
+++ greenlife.client/src/DahsboardPages/EmployeesPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 import EmployeeTable from '../Components/EmployeeList.jsx';
 import Modal from 'react-modal';
 
@@ -16,7 +17,6 @@ function EmployeesPage() {
         { id: 7, name: "Henrique", position: "Frontend Developer 2", department: "TI", isActive: true },
         { id: 8, name: "Marcio", position: "Engenheiro", department: "TI", isActive: true },
         { id: 9, name: "Sophia", position: "PO", department: "Produtos", isActive: true },
-
     ]);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
     const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -31,28 +31,55 @@ function EmployeesPage() {
         setModalIsOpen(false);
     };
 
+    // Defina a animação de entrada e saída para o modal
+    const animation = useSpring({
+        opacity: modalIsOpen ? 1 : 0,
+        transform: modalIsOpen ? `translateY(0%)` : `translateY(-50%)`,
+        config: { tension: 200, friction: 20 },
+    });
+
     return (
         <div>
             <h1 className="font-bold text-3xl mb-8">{empresa} - Lista de funcionarios</h1>
             <EmployeeTable employeeList={employees} handleRowClick={handleRowClick} />
 
-            {selectedEmployee
-                &&
-                (<Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="fixed inset-0 flex items-center justify-center bg-neutral-500 bg-opacity-20" overlayClassName="fixed inset-0 bg-gray-900 bg-opacity-75" >
-                    <div className="bg-neutral-900 p-6 rounded-lg text-white w-96">
-                    <h2 className="font-bold mb-4 text-lg">Detalhes do Funcionario</h2>
-                    <div className="bg-zinc-400 h-0.5 w-full mb-4 my-2"></div>
-                    <p className="mb-2 text-zinc-300"><strong className="text-white">Nome:</strong> {selectedEmployee.name}</p>
-                    <p className="mb-2 text-zinc-300"><strong className="text-white">Posicao:</strong> {selectedEmployee.position}</p>
-                    <p className="mb-2 text-zinc-300"><strong className="text-white">Departamento:</strong> {selectedEmployee.department}</p>
-                    <p className={` mb-4 ${selectedEmployee.isActive ? 'text-green-500' : 'text-red-500'}`} ><strong className="text-white" >Status:</strong> {selectedEmployee.isActive ? "Ativo" : "Inativo"}</p>
+            {selectedEmployee && (
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    className="fixed inset-0 flex items-center justify-center"
+                    overlayClassName="fixed inset-0 bg-neutral-800 bg-opacity-50"
+                    style={{ overlay: { zIndex: 1000 } }}
+                >
+                    {/* Use o componente `animated.div` para aplicar a animação */}
+                    <animated.div style={animation} className="bg-neutral-900 p-6 rounded-lg text-white w-2/5">
+                        <h2 className="font-bold mb-4 text-lg">Detalhes do Funcionario</h2>
+                        <div className="bg-zinc-400 h-0.5 w-full mb-4 my-2"></div>
+                        <p className="mb-2 text-zinc-300">
+                            <strong className="text-white">Nome:</strong> {selectedEmployee.name}
+                        </p>
+                        <p className="mb-2 text-zinc-300">
+                            <strong className="text-white">Posicao:</strong> {selectedEmployee.position}
+                        </p>
+                        <p className="mb-2 text-zinc-300">
+                            <strong className="text-white">Departamento:</strong> {selectedEmployee.department}
+                        </p>
+                        <p className={`mb-4 ${selectedEmployee.isActive ? 'text-green-500' : 'text-red-500'}`}>
+                            <strong className="text-white">Status:</strong>{' '}
+                            {selectedEmployee.isActive ? 'Ativo' : 'Inativo'}
+                        </p>
 
-                    <div className="flex justify-between">
-                        <button className="px-6 py-2 bg-blue-700 text-white rounded mt-4" onClick={closeModal}>Editar</button>
-                        <button className="px-6 py-2 bg-red-700 text-white rounded mt-4" onClick={closeModal}>Fechar</button>
-                    </div>
-                    </div>
-                </Modal>)}
+                        <div className="flex justify-between">
+                            <button className="px-6 py-2 bg-blue-700 text-white rounded mt-4" onClick={closeModal}>
+                                Editar
+                            </button>
+                            <button className="px-6 py-2 bg-red-700 text-white rounded mt-4" onClick={closeModal}>
+                                Fechar
+                            </button>
+                        </div>
+                    </animated.div>
+                </Modal>
+            )}
         </div>
     );
 }
